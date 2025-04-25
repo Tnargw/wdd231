2608e45d1f1807ecf4c3edf5a2662667eeff0a05
Grant Watson <94023954+Tnargw@users.noreply.github.com>
2025-04-25 16:40:04 -0600

    fixed formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// Template for the park info
+function parkInfoTemplate(info) {
+  return `<a href="/" class="location_title">${info.name}</a>
+    <p class="location_states">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+}
+
+// Update the disclaimer with the link and park name
+document.querySelector(".disclaimer > a").href = parkData.url;
+document.querySelector(".disclaimer > a").innerHTML = parkData.fullName;
+
+// Set the page title to the park name
+document.title = parkData.fullName;
+
+// Set the image with the first one
+const img = document.querySelector(".location > img");
+img.src = parkData.images[0].url;
+img.alt = parkData.fullName;
+
+// Put the park data into "location_content"
+document.querySelector(".location_content").innerHTML = parkInfoTemplate(parkData);
