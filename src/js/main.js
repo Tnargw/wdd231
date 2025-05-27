c3fff164b643b2c5402046f83f9e2ef13c797224
Grant Watson <94023954+Tnargw@users.noreply.github.com>
2025-05-27 13:29:07 -0600

    NPS Part 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,30 @@
-import { getParkData, getInfoLinks, getVisitorCenterData } from "./parkService.mjs";
+import "../css/style.css";
+import "../css/home.css";
+
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
+
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
+}
+
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
+  const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
 
 async function init() {
   const parkData = await getParkData();
   const links = getInfoLinks(parkData.images);
-  const visitorCenterData = await getVisitorCenterData();
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(links);
-
-  console.log(visitorCenterData)
 }
 
 init();
-
-
-
-function setParkIntro(data) {
-  const infoElement = document.querySelector(".intro");
-  infoElement.innerHTML = `<h1>${parkData.fullName}</h1>
-  <p>${parkData.description}</p>`;
-}
-
-function setParkInfoLinks(data) {
-  const infoElement = document.querySelector(".info");
-  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
-  const html = data.map(mediaCardTemplate);
-  // join the array of strings into one string and insert it into the section
-  infoElement.insertAdjacentHTML("afterbegin", html.join(""));
-}
-
-
-
-
--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -178,8 +178,7 @@ const park = {
   name: "Yellowstone",
   designation: "National Park"
 };
-
-export const parkInfoLinks = [
+const parkInfoLinks = [
   {
     name: "Current Conditions &#x203A;",
     link: "conditions.html",
@@ -201,16 +200,6 @@ export const parkInfoLinks = [
   }
 ];
 
-
-export function getInfoLinks(data) {
-    // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
-  const withUpdatedImages = parkInfoLinks.map((item, index) => {
-    item.image = data[index + 2].url;
-    return item;
-  });
-  return withUpdatedImages;
-}
-
 const baseUrl = "https://developer.nps.gov/api/v1/";
 const apiKey = import.meta.env.VITE_NPS_API_KEY;
 
@@ -229,12 +218,25 @@ async function getJson(url) {
   return data;
 }
 
+export function getInfoLinks(data) {
+  const withUpdatedImages = parkInfoLinks.map((item, index) => {
+    item.image = data[index].url;
+    return item;
+  });
+  return withUpdatedImages;
+}
+
 export async function getParkData() {
-  const parkData = await getJson("parks?parkCode=yell");
+  const parkData = await getJson("parks?parkCode=yell ");
   return parkData.data[0];
 }
 
-export async function getVisitorCenterData() {
-  const visitorCenterData = await getJson("visitorcenters?parkCode=yell");
-  return visitorCenterData.data[0];
+export async function getParkAlerts(code) {
+  const parkData = await getJson(`alerts?parkCode=${code}`);
+  return parkData.data;
+}
+
+export async function getParkVisitorCenters(code) {
+  const parkData = await getJson(`visitorcenters?parkCode=${code}`);
+  return parkData.data;
 }
